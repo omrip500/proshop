e46e8249238d483b356dd5f45c19d532388e90f6
Omri Peer <omripeer@h-MacBook-Pro-sl-Omri.local>
2025-01-23 10:48:10 +0200

    change shop name

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
           <LinkContainer to="/">
             <Navbar.Brand>
               <img src={logo} alt="ProShop" />
-              Shani's Shop
+              ProShop
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
